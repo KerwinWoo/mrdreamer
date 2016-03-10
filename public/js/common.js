1cd05107ccbc9fb7fb3d9f70efd16a835953dabe
kerwinwoo <672507299@qq.com>
2016-03-11 01:07:12 +0800

    修改了一些目录结构

--- public/js/common.js
+++ public/js/common.js
@@ -2,13 +2,12 @@
  * Created by kerwin on 16/2/23.
  */
 var angularApp = angular.module('app', ['ngRoute','indexModule']);
-/*
+
 angularApp.config(['$routeProvider',function($routeProvider) {
-    $routeProvider.when('/page1', {
-        templateUrl: 'templates/tpl1.html',
+    $routeProvider.when('/#me', {
+        templateUrl: 'templates/jobDetail.jade',
         controller:'Ctrl01'
-    }).
-        otherwise({
+    }).otherwise({
             redirectTo: '/'
         });
-}]);*/
+}]);
--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -3,19 +3,23 @@
  */
 var homeModule = angular.module('homeModule',[]);
 var homeController = homeModule.controller('homeController',['$scope','$http',function($scope, $http){
+    $scope.currentMenuName = '我';
+    $scope.currentJobName = '朗新科技股份有限公司';
 
-    $http.get('data/menu.json').success(function(data){
-        $scope.menuItems = data.items;
+    $http.post('/findAllMenus').success(function(data){
+        $scope.menuItems = data;
         $scope.activeMenu = 'active';
-        $scope.currentMenuId = 1;
-        $scope.menuClick = function(menuId){
-            console.log(menuId);
-            $scope.currentMenuId = menuId;
-        };
     });
 
-    $http.post('/menu').success(function(data){
-        $scope.subMenuItems = data;
+    $http.post('/findAllJobs').success(function(data){
+        $scope.jobs = data;
     });
+
+    $scope.menuClick = function(menuName){
+        $scope.currentMenuName = menuName;
+    };
+    $scope.chooseJob = function(jobName){
+        $scope.currentJobName = jobName;
+    };
 
 }]);
